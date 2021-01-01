fb3f9849fc8437a4d58c79c261a8d51b7157a46c
Alan Bidu <alanbidu@yahoo.com.br>
2021-01-01 02:05:56 -0300

    terceirodia

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ const botao4 = document.querySelector('#botao4');
 const botao5 = document.querySelector('#botao5');
 const botao6 = document.querySelector('#botao6');
 
+let imagens = ["img/java.png", "img/javascript.png", "img/python.png"];
+imagens = imagens.concat(imagens);
+imagens.sort(() => Math.random() - 0.5);
+
 let cartaVirada = "";   //Qual carta foi virada
 let manter = false;   //Manter carta virada
 let botaoAnterior = ""; //Botão clicado anteriormente
+let qtdCartasViradas = 0;
 
 function showHideImg(botao, arquivo) {
     // console.log(botao.querySelector("img").src.split("/")[9]);
@@ -20,9 +25,12 @@ function showHideImg(botao, arquivo) {
         manterCarta(botao, arquivo.slice(4,-4));
     }
     
+    qtdCartasViradas++;
     setTimeout(tempoVisualizacao, 2000, botao);
 
-    botaoAnterior = botao;
+    if(botaoAnterior !== botao){
+        botaoAnterior = botao;
+    }
 }
 
 function manterCarta(botao, imagem)  {
@@ -38,30 +46,34 @@ function manterCarta(botao, imagem)  {
 
 function tempoVisualizacao(botao) {
 
-    if(!manter) {
+    console.log(botaoAnterior);
+    if(!manter && (qtdCartasViradas >1)) {
         botao.innerHTML = `<img src="img/comum.png" alt="comum">`;
         botaoAnterior.innerHTML = `<img src="img/comum.png" alt="comum">`;
+        qtdCartasViradas = 0;
     }
 }
 
 
 botao1.addEventListener('click', function(event) {
     // console.log(event);
-    showHideImg(botao1, "img/java.png");
+    console.log("botao1");
+    showHideImg(botao1, imagens[0]);
     // botao1.virar = 
 });
 botao2.addEventListener('click', function(event) {
-    showHideImg(botao2, "img/javascript.png");
+    console.log("botao2");
+    showHideImg(botao2, imagens[1]);
 });
 botao3.addEventListener('click', function(event) {
-    showHideImg(botao3, "img/python.png");
+    showHideImg(botao3, imagens[2]);
 });
 botao4.addEventListener('click', function(event) {
-    showHideImg(botao4, "img/java.png");
+    showHideImg(botao4, imagens[3]);
 });
 botao5.addEventListener('click', function(event) {
-    showHideImg(botao5, "img/javascript.png");
+    showHideImg(botao5, imagens[4]);
 });
 botao6.addEventListener('click', function(event) {
-    showHideImg(botao6, "img/python.png");
+    showHideImg(botao6, imagens[5]);
 });
